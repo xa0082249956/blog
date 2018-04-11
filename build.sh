adbd9c9d6961ed95efcc8fad11f8f49beae321ad
Haotian Jing <576810415@qq.com>
2018-04-11 22:28:37 +0800

    build.sh: use cd instead of pushd/popd

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 pth=$(pwd)
 go get -v "github.com/xa0082249956/hugo"
-pushd ~/go/src/github.com/xa0082249956/hugo/
+cd ~/go/src/github.com/xa0082249956/hugo/
 # TODO: add builddate
 go build -ldflags "-s -w" -o ~/Github/blog/bin/hugo\
     -o ${pth}/bin/hugo
-popd
+cd ${pth}
 bin/hugo
