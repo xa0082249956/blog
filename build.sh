5f14ab39fd0ded6248cc0557a6f88019ba1e7184
Haotian Jing <576810415@qq.com>
2018-04-11 22:36:58 +0800

    build.sh: use ${GOPATH} as go's root

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 pth=$(pwd)
 go get -v "github.com/xa0082249956/hugo"
-cd ~/go/src/github.com/xa0082249956/hugo/
+cd ${GOPATH}/src/github.com/xa0082249956/hugo/
 # TODO: add builddate
 go build -ldflags "-s -w" -o ~/Github/blog/bin/hugo\
     -o ${pth}/bin/hugo
